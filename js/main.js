a023f53f40bd865d99abbcec061bc0424c08e0bf
josomm22 <josommefeld@icloud.com>
2019-12-02 17:45:44 +0200

    function that genrates items in grid

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let items = {
-    rocks: ['R08C02','R08C03'],
-    wood: ['R08C05','R09C05','R10C05'] 
+    rocks: ['R08C02','R08C03','R08C04','R08C17','R08C18','R09C17','R09C18','R10C17','R10C18',],
+    wood: ['R08C05','R09C05','R10C05'] ,
+    leaves: ['R11C04','R11C05','R11C06','R12C04','R12C05','R12C06','R13C04','R13C05','R13C06','R14C05'],
+    cloud: ['R15C10','R15C11','R15C12','R15C13','R15C14','R15C15','R14C14','R14C15','R16C10','R16C11','R16C12',],
+    water: [],
 }
 function createGrid(){
     let rows = 17;
@@ -40,3 +43,15 @@ function pointsToId (arr){
     colNum = ("0" + col).slice(-2);
     return (`R${rowNum}C${colNum}`)
 }
+
+function displayElements(ElementLibrary){
+    for (el in ElementLibrary){ //iterates through all the properties in the object ElementLibrary
+        for (let i = 0; i < ElementLibrary[el].length; i++) { //itterates over all the items in the array
+            let blockClass = el ;
+            let blockPosition = ElementLibrary[el][i];
+            console.log(`${blockClass} goes to pos ${blockPosition}`);
+            $(`#${blockPosition}`).addClass(`${blockClass}`);
+        }
+    }
+}
+displayElements(items);
