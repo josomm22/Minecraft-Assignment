761a7369746031f91845cc676febd716b6af219a
josomm22 <josommefeld@icloud.com>
2019-12-03 17:27:17 +0200

    force push

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+createGrid();
+
 let items = {
-    rocks: ['R08C02','R08C03'],
-    wood: ['R08C05','R09C05','R10C05'] 
+    rocks: ['R08C02','R08C03','R08C04','R08C17','R08C18','R09C17','R09C18','R10C17','R10C18',],
+    wood: ['R08C05','R09C05','R10C05'] ,
+    leaves: createLeaves('R11C05'),
+    cloud: ['R15C10','R15C11','R15C12','R15C13','R15C14','R15C15','R14C14','R14C15','R16C10','R16C11','R16C12',],
+    water: [],
 }
 function createGrid(){
     let rows = 17;
@@ -27,7 +32,8 @@ function createGrid(){
         $('.container').prepend(rowDiv);
     }
 }
-createGrid();
+
+
 function idToPoints (id){
     let x = parseInt(id.slice(1,3));
     let y = parseInt(id.slice(4));
@@ -41,6 +47,77 @@ function pointsToId (arr){
     return (`R${rowNum}C${colNum}`)
 }
 
+function displayElements(ElementLibrary){
+    for (el in ElementLibrary){ //iterates through all the properties in the object ElementLibrary
+        for (let i = 0; i < ElementLibrary[el].length; i++) { //itterates over all the items in the array
+            let blockClass = el ;
+            let blockPosition = ElementLibrary[el][i];
+            // console.log(`${blockClass} goes to pos ${blockPosition}`);
+            $(`#${blockPosition}`).addClass(`${blockClass}`);
+        }
+    }
+}
+displayElements(items);
+
+function createLeaves(startpoint){
+    let originArr = [startpoint]
+    function grow(){
+        let proba = 0
+        let direction = ['up','left','right']
+        for (let o = 0; o < originArr.length; o++) {
+            for (let i = 0; i < direction.length; i++) {
+                let adjacentBlock = checkAround(originArr[o],direction[i])
+                if (adjacentBlock.className === 'block'){
+                    console.log (`${adjacentBlock.id} is a block`);
+                    if (chance(proba)){
+                        originArr.push(adjacentBlock.id);
+        
+                    }
+                }
+            };
+            proba += 1
+        }
+        console.log(typeof(originArr))
+        let uniqueSet = new Set(originArr)
+        originArr = [...uniqueSet];
+    }
+    grow();
+    console.log(typeof(originArr))
+    console.log(originArr);
+    return originArr;
+  
+    function chance(prob){
+        let win = Math.floor(Math.random() * prob);
+        if (win === 0){
+            return true;
+        };
+         
+    };
+    function checkAround(point,direction){
+        let x = idToPoints(point)[0];
+        let y = idToPoints(point)[1];
+        let left = [x,y-1];
+        let right = [x, y+1];
+        let up = [x+1,y];
+        let move;
+        switch (direction) {
+            case 'left':
+                move = left
+                break;
+            case 'up':
+                move = up
+                break;
+            case 'right':
+                move = right
+                break;
+        
+            default:
+                break;
+        }
+        return (document.getElementById(`${pointsToId(move)}`));
+    }
+}
+    
 let tilesArray = ['dirt','grass', 'rock'];
 let allBlocks = document.getElementsByClassName('block');
 
@@ -93,4 +170,4 @@ function hasAdjacentTile (tileIDmatrix) {
     console.log("filled bottom:", !isEmpty(adjacentBottom));    
     console.log("filled left:", !isEmpty(adjacentLeft));
     return (!isEmpty(adjacentTop) || !isEmpty(adjacentBottom) || !isEmpty(adjacentRight) || !isEmpty(adjacentLeft));    
-}
+};
