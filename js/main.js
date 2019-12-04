ece016fcee779cf10f82eb6c4fa6ae401b0f3a0a
janetranada <jyranada@gmail.com>
2019-12-04 16:57:47 +0200

    edited js for removing tile function

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 createGrid();
 
 let items = {
-    rocks: ['R08C02','R08C03','R08C04','R08C17','R08C18','R09C17','R09C18','R10C17','R10C18',],
-    wood: ['R08C05','R09C05','R10C05'] ,
+    rocks: ['R08C02', 'R08C03', 'R08C04', 'R08C17', 'R08C18', 'R09C17', 'R09C18', 'R10C17', 'R10C18',],
+    wood: ['R08C05', 'R09C05', 'R10C05'],
     leaves: createLeaves('R11C05'),
-    cloud: ['R15C10','R15C11','R15C12','R15C13','R15C14','R15C15','R14C14','R14C15','R16C10','R16C11','R16C12',],
+    cloud: ['R15C10', 'R15C11', 'R15C12', 'R15C13', 'R15C14', 'R15C15', 'R14C14', 'R14C15', 'R16C10', 'R16C11', 'R16C12',],
     water: [],
 }
-function createGrid(){
+function createGrid() {
     let rows = 17;
     let columns = 20;
     for (let r = 0; r <= rows; r++) {
-        rowDiv = $('<div/>').attr('class','row');
+        rowDiv = $('<div/>').attr('class', 'row');
         for (let c = 0; c <= columns; c++) {
             rowNum = ("0" + r).slice(-2);
             colNum = ("0" + c).slice(-2);
-            if (r < 7){
-                colDiv = $('<div/>').attr('class','block dirt');
+            if (r < 7) {
+                colDiv = $('<div/>').attr('class', 'block dirt');
             }
-            else if(r === 7){
-                colDiv = $('<div/>').attr('class','block grass');
+            else if (r === 7) {
+                colDiv = $('<div/>').attr('class', 'block grass');
 
             }
-            else{
-                colDiv = $('<div/>').attr('class','block');
+            else {
+                colDiv = $('<div/>').attr('class', 'block');
             }
-            colDiv.attr('id',`R${rowNum}C${colNum}`)
+            colDiv.attr('id', `R${rowNum}C${colNum}`)
             rowDiv.append(colDiv)
-            
+
         }
         $('.container').prepend(rowDiv);
     }
 }
 
 
-function idToPoints (id){
-    let x = parseInt(id.slice(1,3));
+function idToPoints(id) {
+    let x = parseInt(id.slice(1, 3));
     let y = parseInt(id.slice(4));
-    return [x,y];
+    return [x, y];
 }
-function pointsToId (arr){
+function pointsToId(arr) {
     row = arr[0];
     col = arr[1];
     rowNum = ("0" + row).slice(-2);
@@ -47,6 +47,7 @@ function pointsToId (arr){
     return (`R${rowNum}C${colNum}`)
 }
 
+let selectedTool = "pickaxe";
 //let pressedMouse = false;
 let axe = false;
 let shovel = false;
@@ -55,140 +56,197 @@ let pickaxe = false;
 
 let box = document.getElementsByClassName("block");
 
-document.getElementById("axe").addEventListener('click', function(event){
+document.getElementById("axe").addEventListener('click', function (event) {
     axe = true;
     shovel = false;
     pickaxe = false;
 });
 
 
-document.getElementById("shovel").addEventListener('click', function(event){
+document.getElementById("shovel").addEventListener('click', function (event) {
     axe = false;
     shovel = true;
     pickaxe = false;
 });
 
 
-document.getElementById("pickaxe").addEventListener('click', function(event){
+document.getElementById("pickaxe").addEventListener('click', function (event) {
     axe = false;
     shovel = false;
     pickaxe = true;
 });
 
-//grass
-box.addEventListener('click',function(){
+let tools = {
+    axe: ["wood", "leaves"],
+    pickaxe: ["rocks"],
+    shovel: ["dirt", "grass", "leaves"]
+}
 
-    if (box.classList('grass') && shovel){
-      
-        box.classList.remove("grass");
-    }else{
-            box.addEventListener("mousedown",function(e){
-                //pressedMouse = true;
-                box.classList.add("background-red");
-            });
-            box.addEventListener("onmousemove", function(e){
-                //pressedMouse = true;
-                box.classList.add("background-red");
-            });
-            box.addEventListener("mouseup", function(e){
-                //pressedMouse = false;
-                box.classList.remove("background-red");
-            });
+
+function flashBgRed() {
+    $(`#${selectedTool}`).toggleClass('bg-red');
+}
+
+function takeTileOut(tileID) {
+    let targetTile = document.getElementById(tileID);
+    tools[selectedTool].forEach(element => {
+        if (targetTile.classList.contains(element)) {
+            targetTile.classList.remove(element);
+        } else {
+            for (let i = 0; i < 4; i++) {
+                setTimeout(flashBgRed, 1000 * i);
+            }
         }
-});
-//dirt
-box.addEventListener('click',function(){
-
-    if (box.classList("dirt") && shovel){
-    
-    box.remove.classList("dirt");
-    }else{
-        box.addEventListener("mousedown",function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("onmousemove", function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("mouseup", function(e){
-            //pressedMouse = false;
-            box.classList.remove("background-red");
-        });
-    }   
-    
-});
-//tree
-box.addEventListener('click',function(){
-
-    if (box.classList("tree") && axe){ 
- 
-    box.remove.classList("tree");
-    }else{
-        box.addEventListener("mousedown",function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("onmousemove", function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("mouseup", function(e){
-            //pressedMouse = false;
-            box.classList.remove("background-red");
-        });
     }
-    
-});
-//leave
-box.addEventListener('click',function(){
+    )
+};
 
-    if (box.classList("leave") && axe){ 
+// 	switch(activeTool) {
+// 		case "shovel":
+//             tools[activeTool].forEach(function(){
+//                 if (targetTile.classList.contains(activeToolEl)) {
+//                     targetTile.classList.remove(actoveToolEl);
+//                 } else {
+//                     for(let i = 0; i < 4; i++) {
+//                         setTimeout(flashBgRed(shovel), 1000 * i);
+//                     }   
+//                 }
+//             }
+//             break;
 
-    box.remove.classList("leave");
-    }else{
-        box.addEventListener("mousedown",function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("onmousemove", function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("mouseup", function(e){
-            //pressedMouse = false;
-            box.classList.remove("background-red");
-        });
-    }
-    
-});
-//rock
-box.addEventListener('click',function(){
 
-    if (box.classList("rock") && pickaxe){ 
-  
-    box.remove.classList("rock");
-    }else{
-        box.addEventListener("mousedown",function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("onmousemove", function(e){
-            //pressedMouse = true;
-            box.classList.add("background-red");
-        });
-        box.addEventListener("mouseup", function(e){
-            //pressedMouse = false;
-            box.classList.remove("background-red");
-        });
-    }
-    
-});
 
-function displayElements(ElementLibrary){
-    for (el in ElementLibrary){ //iterates through all the properties in the object ElementLibrary
+// 		case "axe":
+// 			if (targetTile.classList.contains('wood') || targetTile.classList.contains('leaves')) {
+//                 targetTile.classList.remove('wood');
+//                 targetTile.classList.remove('leaves');
+// 			} else {
+//                 for(let i = 0; i < 4; i++) {
+//                     setTimeout(flashBgRed(shovel), 1000 * i);
+//                 }   
+//             }
+
+
+
+// 	}
+
+
+// }
+
+// //grass
+// box.addEventListener('click', function () {
+
+//     if (box.classList('grass') && shovel) {
+
+//         box.classList.remove("grass");
+//     } else {
+//         box.addEventListener("mousedown", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("onmousemove", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("mouseup", function (e) {
+//             //pressedMouse = false;
+//             box.classList.remove("background-red");
+//         });
+//     }
+// });
+// //dirt
+// box.addEventListener('click', function () {
+
+//     if (box.classList("dirt") && shovel) {
+
+//         box.remove.classList("dirt");
+//     } else {
+//         box.addEventListener("mousedown", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("onmousemove", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("mouseup", function (e) {
+//             //pressedMouse = false;
+//             box.classList.remove("background-red");
+//         });
+//     }
+
+// });
+// //tree
+// box.addEventListener('click', function () {
+
+//     if (box.classList("tree") && axe) {
+
+//         box.remove.classList("tree");
+//     } else {
+//         box.addEventListener("mousedown", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("onmousemove", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("mouseup", function (e) {
+//             //pressedMouse = false;
+//             box.classList.remove("background-red");
+//         });
+//     }
+
+// });
+// //leave
+// box.addEventListener('click', function () {
+
+//     if (box.classList("leave") && axe) {
+
+//         box.remove.classList("leave");
+//     } else {
+//         box.addEventListener("mousedown", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("onmousemove", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("mouseup", function (e) {
+//             //pressedMouse = false;
+//             box.classList.remove("background-red");
+//         });
+//     }
+
+// });
+// //rock
+// box.addEventListener('click', function () {
+
+//     if (box.classList("rock") && pickaxe) {
+
+//         box.remove.classList("rock");
+//     } else {
+//         box.addEventListener("mousedown", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("onmousemove", function (e) {
+//             //pressedMouse = true;
+//             box.classList.add("background-red");
+//         });
+//         box.addEventListener("mouseup", function (e) {
+//             //pressedMouse = false;
+//             box.classList.remove("background-red");
+//         });
+//     }
+
+// });
+
+function displayElements(ElementLibrary) {
+    for (el in ElementLibrary) { //iterates through all the properties in the object ElementLibrary
         for (let i = 0; i < ElementLibrary[el].length; i++) { //itterates over all the items in the array
-            let blockClass = el ;
+            let blockClass = el;
             let blockPosition = ElementLibrary[el][i];
             // console.log(`${blockClass} goes to pos ${blockPosition}`);
             $(`#${blockPosition}`).addClass(`${blockClass}`);
@@ -197,19 +255,19 @@ function displayElements(ElementLibrary){
 }
 displayElements(items);
 
-function createLeaves(startpoint){
+function createLeaves(startpoint) {
     let originArr = [startpoint]
-    function grow(){
+    function grow() {
         let proba = 0
-        let direction = ['up','left','right']
+        let direction = ['up', 'left', 'right']
         for (let o = 0; o < originArr.length; o++) {
             for (let i = 0; i < direction.length; i++) {
-                let adjacentBlock = checkAround(originArr[o],direction[i])
-                if (adjacentBlock.className === 'block'){
+                let adjacentBlock = checkAround(originArr[o], direction[i])
+                if (adjacentBlock.className === 'block') {
                     // console.log (`${adjacentBlock.id} is a block`);
-                    if (chance(proba)){
+                    if (chance(proba)) {
                         originArr.push(adjacentBlock.id);
-        
+
                     }
                 }
             };
@@ -223,20 +281,20 @@ function createLeaves(startpoint){
     // console.log(typeof(originArr))
     // console.log(originArr);
     return originArr;
-  
-    function chance(prob){
+
+    function chance(prob) {
         let win = Math.floor(Math.random() * prob);
-        if (win === 0){
+        if (win === 0) {
             return true;
         };
-         
+
     };
-    function checkAround(point,direction){
+    function checkAround(point, direction) {
         let x = idToPoints(point)[0];
         let y = idToPoints(point)[1];
-        let left = [x,y-1];
-        let right = [x, y+1];
-        let up = [x+1,y];
+        let left = [x, y - 1];
+        let right = [x, y + 1];
+        let up = [x + 1, y];
         let move;
         switch (direction) {
             case 'left':
@@ -248,64 +306,73 @@ function createLeaves(startpoint){
             case 'right':
                 move = right
                 break;
-        
+
             default:
                 break;
         }
         return (document.getElementById(`${pointsToId(move)}`));
     }
 }
-    
-let tilesArray = ['dirt','grass', 'rocks', 'wood', 'leaves'];
+
+let tilesArray = ['dirt', 'grass', 'rocks', 'wood', 'leaves'];
 let allBlocks = document.getElementsByClassName('block');
 
-Array.from(allBlocks).forEach(singleBlock => singleBlock.addEventListener('click', function(){
-    tileID = this.id; 
-    tileIDmatrix = idToPoints(tileID);
-    console.log("tile ID:", tileID, "/", "tile ID matrix:", tileIDmatrix);
-    console.log("can implant?", canImplant(tileID, tileIDmatrix));   
-    // return canImplant(tileID, tileIDmatrix);  --remove console log later and retain this return t/f
+Array.from(allBlocks).forEach(singleBlock => singleBlock.addEventListener('click', function () {
+    tileID = this.id;
+    bridgeFunc();
 }));
 
-function canImplant (tileID, tileIDmatrix) {    
-    if (isEmpty(tileID)){
+
+function bridgeFunc(tileID) {
+    tileIDmatrix = idToPoints(tileID);
+
+    // if tools=active
+    takeTileOut(tileID);
+
+    //if inventory=active
+    return canImplant(tileID, tileIDmatrix);
+
+}
+
+function canImplant(tileID, tileIDmatrix) {
+    if (isEmpty(tileID)) {
         console.log('tile is empty')
-        return hasAdjacentTile(tileIDmatrix);        
+        return hasAdjacentTile(tileIDmatrix);
     } else {
         console.log('tile is occupied');
         hasAdjacentTile(tileIDmatrix);
         return false;
-    } 
+    }
 }
 
 
 function isEmpty(tileID) {
     tileToCheck = document.getElementById(tileID);
     let empty = true;
-    
+
     for (let i = 0; i < tilesArray.length; i++) {
         try {
-            if(tileToCheck.classList.contains(tilesArray[i])) {
+            if (tileToCheck.classList.contains(tilesArray[i])) {
                 empty = false;
                 return empty;
-            }; 
+            };
         } catch (error) {
             return "error";
-        }                  
+        }
     }
     return empty;
-  
+
 }
 
-function hasAdjacentTile (tileIDmatrix) {
+function hasAdjacentTile(tileIDmatrix) {
     //get adjacents' matrices and convert to ID strings
-    let adjacentTop = pointsToId([tileIDmatrix[0] + 1,  tileIDmatrix[1]]);    
-    let adjacentBottom = pointsToId([tileIDmatrix[0] - 1,  tileIDmatrix[1]]);
-    let adjacentRight = pointsToId([tileIDmatrix[0],  tileIDmatrix[1] + 1]);
-    let adjacentLeft = pointsToId([tileIDmatrix[0],  tileIDmatrix[1] - 1]);
+    let adjacentTop = pointsToId([tileIDmatrix[0] + 1, tileIDmatrix[1]]);
+    let adjacentBottom = pointsToId([tileIDmatrix[0] - 1, tileIDmatrix[1]]);
+    let adjacentRight = pointsToId([tileIDmatrix[0], tileIDmatrix[1] + 1]);
+    let adjacentLeft = pointsToId([tileIDmatrix[0], tileIDmatrix[1] - 1]);
     console.log("filled top:", !isEmpty(adjacentTop));
     console.log("filled right:", !isEmpty(adjacentRight));
-    console.log("filled bottom:", !isEmpty(adjacentBottom));    
+    console.log("filled bottom:", !isEmpty(adjacentBottom));
     console.log("filled left:", !isEmpty(adjacentLeft));
-    return (!isEmpty(adjacentTop) || !isEmpty(adjacentBottom) || !isEmpty(adjacentRight) || !isEmpty(adjacentLeft));    
+    return (!isEmpty(adjacentTop) || !isEmpty(adjacentBottom) || !isEmpty(adjacentRight) || !isEmpty(adjacentLeft));
 };
